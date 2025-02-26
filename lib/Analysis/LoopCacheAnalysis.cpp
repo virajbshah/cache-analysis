72547374d4c85cdc480dc9e3f31f2bd389db1efa
Viraj Shah <shahvirajbiren@gmail.com>
2025-02-26 17:54:14 +0530

    Add support for more load and store operations.

--- lib/Analysis/LoopCacheAnalysis.cpp
+++ lib/Analysis/LoopCacheAnalysis.cpp
@@ -8,6 +8,7 @@
 
 #include "mlir/Dialect/Affine/Analysis/AffineStructures.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/IR/AffineExpr.h"
 #include "mlir/IR/BuiltinTypes.h"
@@ -55,6 +56,25 @@ void LoopCacheAnalysis::runOnOperation() {
   }
 }
 
+namespace {
+
+template <typename LoadOrStoreOpTy>
+AffineMap getAffineMap(LoadOrStoreOpTy LoadOrStoreOp) {
+  return AffineMap::getMultiDimIdentityMap(
+      LoadOrStoreOp.getMemRefType().getShape().size(),
+      LoadOrStoreOp.getContext());
+}
+
+template <> AffineMap getAffineMap(affine::AffineLoadOp LoadOrStoreOp) {
+  return LoadOrStoreOp.getAffineMap();
+}
+
+template <> AffineMap getAffineMap(affine::AffineStoreOp LoadOrStoreOp) {
+  return LoadOrStoreOp.getAffineMap();
+}
+
+} // namespace
+
 llvm::Expected<llvm::SmallVector<AffineExpr>>
 LoopCacheAnalysis::getLoopCacheCost(affine::AffineForOp *LoopOp) {
   auto LoopNest = getLoopNestAsList(LoopOp);
@@ -76,66 +96,78 @@ LoopCacheAnalysis::getLoopCacheCost(affine::AffineForOp *LoopOp) {
                                                InnerMostLoop->getContext());
 
   llvm::DenseMap<void *, llvm::SmallVector<AffineExpr>> RefGroups;
+  auto ProcessAffineLoadOrStoreOp = [&](auto LoadOrStoreOp) {
+    // Let's skip more interesting layouts for now.
+    if (!LoadOrStoreOp.getMemRefType().getLayout().isIdentity()) {
+      LLVM_DEBUG(llvm::outs() << "unsupported layout");
+      return;
+    }
+
+    // Add symbols representing the cache line size and total cache size
+    // at positions 0 and 1 respectively.
+    AffineMap MappedIndices = getAffineMap(LoadOrStoreOp).shiftSymbols(2);
+    const auto &[Strides, Offset] =
+        LoadOrStoreOp.getMemRefType().getStridesAndOffset();
+
+    llvm::SmallVector<unsigned> Levels;
+    for (Value Index :
+         LoadOrStoreOp.getIndices().take_front(MappedIndices.getNumDims())) {
+      unsigned Level = IndVarIndex[Index];
+      Levels.push_back(Level);
+    }
+
+    auto LevelToIndexMap = AffineMap::getMultiDimMapWithTargets(
+        LoopNest->size(), Levels, InnerMostLoop->getContext());
+    auto MappedLevels = MappedIndices.compose(LevelToIndexMap);
+
+    AffineExpr LinearizedIndex =
+        getAffineConstantExpr(0, LoadOrStoreOp->getContext());
+    for (const auto &[MappedLevel, Stride] :
+         llvm::zip_equal(MappedLevels.getResults(), Strides)) {
+      LinearizedIndex = LinearizedIndex + MappedLevel * Stride;
+    }
+
+    void *BasePointer = LoadOrStoreOp.getMemref().getAsOpaquePointer();
+    // Assuming the base pointer is aligned.
+    AffineExpr CacheLineExpr = LinearizedIndex.floorDiv(getAffineSymbolExpr(
+        CacheLineSizeSymbolIndex, LoadOrStoreOp->getContext()));
+    if (RefGroups.contains(BasePointer)) {
+      bool IsCacheLineReuse = false;
+      for (auto &RefGroup : RefGroups[BasePointer]) {
+        AffineExpr Distance = CacheLineExpr - RefGroup;
+        // TODO: Consider cases where it might be zero based on some
+        // constraint on the values of symbols.
+        if (Distance == 0) {
+          IsCacheLineReuse = true;
+          break;
+        }
+      }
+      if (IsCacheLineReuse) {
+        // Do not consider this access in the cost computation.
+        return;
+      }
+    } else {
+      RefGroups[BasePointer] = llvm::SmallVector<AffineExpr>{CacheLineExpr};
+    }
+
+    auto TotalDistance =
+        LinearizedIndex.replaceDims(InnerMostStep) *
+        (LoadOrStoreOp.getMemRefType().getElementTypeBitWidth() / 8);
+    CacheCosts.push_back(TotalDistance);
+  };
+
   for (auto &Op : InnerMostLoop.getOps()) {
     // For now, treat loads and stores similarly.
-    if (auto LoadOp = llvm::dyn_cast<affine::AffineLoadOp>(Op)) {
-      // Let's skip more interesting layouts for now.
-      if (!LoadOp.getMemRefType().getLayout().isIdentity()) {
-        LLVM_DEBUG(llvm::outs() << "unsupported layout");
-        continue;
-      }
-
-      // Add symbols representing the cache line size and total cache size
-      // at positions 0 and 1 respectively.
-      AffineMap MappedIndices = LoadOp.getAffineMap().shiftSymbols(2);
-      const auto &[Strides, Offset] =
-          LoadOp.getMemRefType().getStridesAndOffset();
-
-      llvm::SmallVector<unsigned> Levels;
-      for (Value Index :
-           LoadOp.getIndices().take_front(MappedIndices.getNumDims())) {
-        unsigned Level = IndVarIndex[Index];
-        Levels.push_back(Level);
-      }
-
-      auto LevelToIndexMap = AffineMap::getMultiDimMapWithTargets(
-          LoopNest->size(), Levels, InnerMostLoop->getContext());
-      auto MappedLevels = MappedIndices.compose(LevelToIndexMap);
-
-      AffineExpr LinearizedIndex =
-          getAffineConstantExpr(0, LoadOp->getContext());
-      for (const auto &[MappedLevel, Stride] :
-           llvm::zip_equal(MappedLevels.getResults(), Strides)) {
-        LinearizedIndex = LinearizedIndex + MappedLevel * Stride;
-      }
-
-      void *BasePointer = LoadOp.getMemref().getAsOpaquePointer();
-      // Assuming the base pointer is aligned.
-      AffineExpr CacheLineExpr = LinearizedIndex.floorDiv(
-          getAffineSymbolExpr(CacheLineSizeSymbolIndex, LoadOp->getContext()));
-      if (RefGroups.contains(BasePointer)) {
-        bool IsCacheLineReuse = false;
-        for (auto &RefGroup : RefGroups[BasePointer]) {
-          AffineExpr Distance = CacheLineExpr - RefGroup;
-          // TODO: Consider cases where it might be zero based on some
-          // constraint on the values of symbols.
-          if (Distance == 0) {
-            IsCacheLineReuse = true;
-            break;
-          }
-        }
-        if (IsCacheLineReuse) {
-          // Do not consider this access in the cost computation.
-          continue;
-        }
-      } else {
-        RefGroups[BasePointer] = llvm::SmallVector<AffineExpr>{CacheLineExpr};
-      }
-
-      auto TotalDistance =
-          LinearizedIndex.replaceDims(InnerMostStep) *
-          (LoadOp.getMemRefType().getElementTypeBitWidth() / 8);
-      CacheCosts.push_back(TotalDistance);
+    if (auto AffineLoadOp = dyn_cast<affine::AffineLoadOp>(Op)) {
+      ProcessAffineLoadOrStoreOp(AffineLoadOp);
+    } else if (auto AffineStoreOp = dyn_cast<affine::AffineStoreOp>(Op)) {
+      ProcessAffineLoadOrStoreOp(AffineStoreOp);
+    } else if (auto LoadOp = dyn_cast<memref::LoadOp>(Op)) {
+      // TODO: Process regular loads.
+      ProcessAffineLoadOrStoreOp(LoadOp);
+    } else if (auto StoreOp = dyn_cast<memref::StoreOp>(Op)) {
+      // TODO: Process regular stores.
+      ProcessAffineLoadOrStoreOp(StoreOp);
     }
   }
 
